99c79f1b9d3d112b8f9fe442857168c4aeb62788
Luboš Krčál <lubos.krcal@gmail.com>
2021-09-16 00:06:59 +1200

    Update parameters and EDS file filters in experiments script

--- experiments.bash
+++ experiments.bash
@@ -6,9 +6,10 @@ REPEATS=100
 
 echo "filename,dataset,deg_prob,rec_prob,seg_size_avg,elm_len_avg,len,repeats,pattern_length,algorithm,total_time" > "$RESULTS_FILE"
 
-EDS_FILE_PATTERN="*.eds"
+# Run BNDM algorithm on all EDS data, using randomly generated pattern
+EDS_FILE_PATTERN="synth-*.eds"
 PATTERN_LENGTH=32
-ALGORITHM=1
+ALGORITHM="bndm"
 find "$EDS_DIRECTORY" -type f -name "$EDS_FILE_PATTERN" -print0 | while read -d $'\0' FILE
 do
     echo "Running BNDM-EDS: ./eds_search \"$FILE\" \"$REPEATS\" \"$PATTERN_LENGTH\" \"$ALGORITHM\"..."
@@ -18,11 +19,12 @@ do
     ./eds_search "$FILE" "$REPEATS" "$PATTERN_LENGTH" "$ALGORITHM" | sed -n "s/Total time:\s*\([0-9.]*\) s/\1/p" >> "$RESULTS_FILE"
 done
 
-EDS_FILE_PATTERN="synth-protein_*.eds"
+# Run BNDM-AA algorithm on DNA EDS data (skip protein EDS data), using static pattern
+PROTEIN_FILE_PATTERN="synth-protein_*.eds"
 PATTERN_LENGTH=10
 PATTERN="QRGKGTCDQK" # Expands to 2 DNA patterns
-ALGORITHM=3
-find "$EDS_DIRECTORY" -type f -name "$EDS_FILE_PATTERN" -print0 | while read -d $'\0' FILE
+ALGORITHM="bndm-aa"
+find "$EDS_DIRECTORY" -type f -name "$EDS_FILE_PATTERN" ! -name "$PROTEIN_FILE_PATTERN" -print0 | while read -d $'\0' FILE
 do
     echo "Running BNDM-EDS: ./eds_search \"$FILE\" \"$REPEATS\" \"$PATTERN_LENGTH\" \"$ALGORITHM\" \"$PATTERN\"..."
     echo -n "\"$FILE\"," >> "$RESULTS_FILE"
