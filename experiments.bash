7a5cf834127e199701ea213245221e81dcd1b831
Luboš Krčál <lubos.krcal@gmail.com>
2021-09-28 17:16:53 +1300

    Add more fixed pattern experiment configurations

--- experiments.bash
+++ experiments.bash
@@ -4,13 +4,14 @@ EDS_DIRECTORY="../vcf2eds/scripts/"
 EDS_FILE_PATTERN="synth-*.eds"
 PROTEIN_FILE_PATTERN="synth-protein_*.eds"
 NODEG_FILE_PATTERN="synth-nodeg*.eds"
+SEG_SIZE_FILE_PATTERN="synth-seg-size-avg*.eds"
 RESULTS_FILE="results_v2.csv"
-REPEATS=1000
+REPEATS=10
 
 echo "filename,dataset,deg_prob,rec_prob,seg_size_avg,elm_len_avg,len,repeats,pattern,pattern_length,algorithm,total_time" > "$RESULTS_FILE"
 
-# Run BNDM algorithm on all EDS data, using randomly generated pattern
-PATTERN_LENGTH=32
+# Run BNDM algorithm on all EDS data (DNA and protein), using randomly generated pattern
+PATTERN_LENGTH=30
 ALGORITHM="bndm"
 find "$EDS_DIRECTORY" -type f -name "$EDS_FILE_PATTERN" ! -name "$NODEG_FILE_PATTERN" -print0 | while read -d $'\0' FILE
 do
@@ -35,7 +36,8 @@ do
   done < patterns_aa.list
 done
 
-# Run BNDM-AA and BNDM-EDS on non-degenerate date - to compare BNDM performance, fixed pattern (used twice in BNDM-AA)
+# Run BNDM-AA and BNDM-EDS on non-degenerate data - to compare BNDM performance, using fixed DNA pattern (the same
+# pattern is used as both patterns in BNDM-AA)
 PATTERN="TTTAATCATGAGCTGTTGCATACGGCGC" # Single pattern to be used
 ALGORITHMS="bndm bndm-aa"
 find "$EDS_DIRECTORY" -type f -name "$NODEG_FILE_PATTERN" -print0 | while read -d $'\0' FILE
