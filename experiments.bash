b4bf5274c673592dcfb9f517337fee2bf81e10f2
Luboš Krčál <lubos.krcal@gmail.com>
2021-09-29 14:13:30 +1300

    Generalize experiments script

--- experiments.bash
+++ experiments.bash
@@ -2,37 +2,52 @@
 
 EDS_DIRECTORY="../vcf2eds/scripts/"
 EDS_FILE_PATTERN="synth-*.eds"
+DNAFILE_PATTERN="synth-dna_*.eds"
 PROTEIN_FILE_PATTERN="synth-protein_*.eds"
 NODEG_FILE_PATTERN="synth-nodeg*.eds"
 SEG_SIZE_FILE_PATTERN="synth-seg-size-avg*.eds"
 RESULTS_FILE="results_v2.csv"
-REPEATS=10
+REPEATS=1
 
-echo "filename,dataset,deg_prob,rec_prob,seg_size_avg,elm_len_avg,len,repeats,pattern,pattern_length,algorithm,total_time" > "$RESULTS_FILE"
+if [[ ! -f "$RESULTS_FILE" ]]; then
+  echo "filename,dataset,deg_prob,rec_prob,seg_size_avg,elm_len_avg,len,repeats,pattern,pattern_length,algorithm,total_time" > "$RESULTS_FILE"
+fi
+
+run_eds_search () {
+  local FILE="$1"
+  local REPEATS="$2"
+  local PATTERN_LENGTH="$3"
+  local ALGORITHM="$4"
+  local PATTERN="$5"
+  local PATTERN2="$6"
+
+  echo "Running BNDM-EDS: ./eds_search \"$FILE\" \"$REPEATS\" \"$PATTERN_LENGTH\" \"$ALGORITHM\" \"$PATTERN\" \"$PATTERN2\"..."
+  echo -n "\"$FILE\"," >> "$RESULTS_FILE"
+  echo -n "$FILE" | sed -n 's/.*\/\([^\/]*\)_p=\([0-9.]*\)_r=\([0-9.]*\)_s=\([0-9.]*\)_e=\([0-9.]*\)_l=0*\([0-9]*\)\.eds/\1,\2,\3,\4,\5,\6,/p' >> "$RESULTS_FILE"
+  echo -n "$REPEATS,$PATTERN,$PATTERN_LENGTH,$ALGORITHM," >> "$RESULTS_FILE"
+  ./eds_search "$FILE" "$REPEATS" "$PATTERN_LENGTH" "$ALGORITHM" "$PATTERN" "$PATTERN2" | sed -n "s/Total time:\s*\([0-9.]*\) s/\1/p" >> "$RESULTS_FILE"
+}
 
 # Run BNDM algorithm on all EDS data (DNA and protein), using randomly generated pattern
-PATTERN_LENGTH=30
-ALGORITHM="bndm"
 find "$EDS_DIRECTORY" -type f -name "$EDS_FILE_PATTERN" ! -name "$NODEG_FILE_PATTERN" -print0 | while read -d $'\0' FILE
 do
-    echo "Running BNDM-EDS: ./eds_search \"$FILE\" \"$REPEATS\" \"$PATTERN_LENGTH\" \"$ALGORITHM\"..."
-    echo -n "\"$FILE\"," >> "$RESULTS_FILE"
-    echo -n "$FILE" | sed -n 's/.*\/\([^\/]*\)_p=\([0-9.]*\)_r=\([0-9.]*\)_s=\([0-9.]*\)_e=\([0-9.]*\)_l=0*\([0-9]*\)\.eds/\1,\2,\3,\4,\5,\6,/p' >> "$RESULTS_FILE"
-    echo -n "$REPEATS,,$PATTERN_LENGTH,$ALGORITHM," >> "$RESULTS_FILE"
-    ./eds_search "$FILE" "$REPEATS" "$PATTERN_LENGTH" "$ALGORITHM" | sed -n "s/Total time:\s*\([0-9.]*\) s/\1/p" >> "$RESULTS_FILE"
+    run_eds_search "$FILE" "$REPEATS" "30" "bndm"
 done
 
-# Run BNDM-AA algorithm on DNA EDS data (skip protein EDS data)
-ALGORITHM="bndm-aa"
+# Run BNDM-AA algorithm on all EDS data (DNA only), using fixed AA pattern
+PATTERN="QRGKGTCQKL"
+PATTERN_LENGTH=${#PATTERN}
 find "$EDS_DIRECTORY" -type f -name "$EDS_FILE_PATTERN" ! -name "$PROTEIN_FILE_PATTERN" ! -name "$NODEG_FILE_PATTERN" -print0 | while read -d $'\0' FILE
+do
+    run_eds_search "$FILE" "$REPEATS" "$PATTERN_LENGTH" "bndm-aa" "$PATTERN"
+done
+
+# Run BNDM-AA algorithm on basic DNA EDS data, with varying AA patterns
+find "$EDS_DIRECTORY" -type f -name "$DNAFILE_PATTERN" -print0 | while read -d $'\0' FILE
 do
   while read PATTERN; do
     PATTERN_LENGTH=${#PATTERN}
-    echo "Running BNDM-EDS: ./eds_search \"$FILE\" \"$REPEATS\" \"$PATTERN_LENGTH\" \"$ALGORITHM\" \"$PATTERN\"..."
-    echo -n "\"$FILE\"," >> "$RESULTS_FILE"
-    echo -n "$FILE" | sed -n 's/.*\/\([^\/]*\)_p=\([0-9.]*\)_r=\([0-9.]*\)_s=\([0-9.]*\)_e=\([0-9.]*\)_l=0*\([0-9]*\)\.eds/\1,\2,\3,\4,\5,\6,/p' >> "$RESULTS_FILE"
-    echo -n "$REPEATS,$PATTERN,$PATTERN_LENGTH,$ALGORITHM," >> "$RESULTS_FILE"
-    ./eds_search "$FILE" "$REPEATS" "$PATTERN_LENGTH" "$ALGORITHM" "$PATTERN" | sed -n "s/Total time:\s*\([0-9.]*\) s/\1/p" >> "$RESULTS_FILE"
+    run_eds_search "$FILE" "$REPEATS" "$PATTERN_LENGTH" "bndm-aa" "$PATTERN"
   done < patterns_aa.list
 done
 
@@ -44,10 +59,6 @@ find "$EDS_DIRECTORY" -type f -name "$NODEG_FILE_PATTERN" -print0 | while read -
 do
   for ALGORITHM in $ALGORITHMS; do
     PATTERN_LENGTH=${#PATTERN}
-    echo "Running BNDM-EDS: ./eds_search \"$FILE\" \"$REPEATS\" \"$PATTERN_LENGTH\" \"$ALGORITHM\" \"$PATTERN\" \"$PATTERN\"..."
-    echo -n "\"$FILE\"," >> "$RESULTS_FILE"
-    echo -n "$FILE" | sed -n 's/.*\/\([^\/]*\)_p=\([0-9.]*\)_r=\([0-9.]*\)_s=\([0-9.]*\)_e=\([0-9.]*\)_l=0*\([0-9]*\)\.eds/\1,\2,\3,\4,\5,\6,/p' >> "$RESULTS_FILE"
-    echo -n "$REPEATS,$PATTERN,$PATTERN_LENGTH,$ALGORITHM," >> "$RESULTS_FILE"
-    ./eds_search "$FILE" "$REPEATS" "$PATTERN_LENGTH" "$ALGORITHM" "$PATTERN" "$PATTERN" | sed -n "s/Total time:\s*\([0-9.]*\) s/\1/p" >> "$RESULTS_FILE"
+    run_eds_search "$FILE" "$REPEATS" "$PATTERN_LENGTH" "$ALGORITHM" "$PATTERN" "$PATTERN"
   done
 done
