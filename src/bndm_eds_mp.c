6b0ff425aaf3190a8a2a3287f44a4d2f96d26344
Luboš Krčál <lubos.krcal@gmail.com>
2021-10-13 14:18:24 +1300

    Generalize BNDM-EDS-MP for variable amount of IUPAC patterns

--- src/bndm_eds_mp.c
+++ src/bndm_eds_mp.c
@@ -1,3 +1,4 @@
+#include <assert.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
@@ -8,6 +9,18 @@
 #include "protein_table.h"
 
 #include "bndm_eds_mp.h"
+
+// If USE_STATIC_NUM_PATTERNS is defined, all the pattern loops will use a constant number of patterns set to
+// MAX_DNA_PATTERNS, even if the actual runtime number of patterns is lower. This is to allow the compiler optimize
+// the code using loop unrolling, which is not possible for runtime-dependent number of patterns.
+
+#define USE_STATIC_NUM_PATTERNS
+
+#ifdef USE_STATIC_NUM_PATTERNS
+#define NUM_PATTERNS (MAX_DNA_PATTERNS)
+#else
+#define NUM_PATTERNS (num_patterns)
+#endif
 
 int bndm_eds_mp_run(const unsigned char *teds,
                     const size_t len,
@@ -23,18 +36,20 @@ int bndm_eds_mp_run(const unsigned char *teds,
         return 1;
     }
     unsigned char IUPAC_patterns[MAX_DNA_PATTERNS][MAX_PATTERN_LENGTH];
-    int num_dna_patterns = translate_aa_iupac_all_combinations(pattern, m, IUPAC_patterns,
-                                                               MAX_DNA_PATTERNS, MAX_PATTERN_LENGTH);
-    if (num_dna_patterns==0) {
+    size_t num_iupac_patterns = translate_aa_iupac_all_combinations(pattern, m, IUPAC_patterns,
+                                                                    MAX_DNA_PATTERNS, MAX_PATTERN_LENGTH);
+    if (num_iupac_patterns==0) {
         fprintf(stderr, "BNDM-EDS-MP failed to generate any DNA patterns!\n");
         return 1;
-    } else if (num_dna_patterns==1) {
-        memcpy(IUPAC_patterns[1], IUPAC_patterns[1], MAX_PATTERN_LENGTH);
+    }
+    for (size_t i = num_iupac_patterns; i < MAX_DNA_PATTERNS; i++) {
+        memset(IUPAC_patterns[i], 0, sizeof(unsigned char)*MAX_PATTERN_LENGTH);
     }
 
     printf("BNDM-EDS-MP (Multi-Patterns generated from AA)\n");
-    printf("Pattern0:\t%.*s\n", (int)m*3, IUPAC_patterns[0]);
-    printf("Pattern1:\t%.*s\n", (int)m*3, IUPAC_patterns[1]);
+    for (size_t i = 0; i < num_iupac_patterns; i++) {
+        printf("Pattern%ld:\t\"%.*s\"\n", i, (int) m*3, IUPAC_patterns[i]);
+    }
 
     getrusage(RUSAGE_SELF, &ruse);
     ssec1 = (double) (ruse.ru_stime.tv_sec*1000000 + ruse.ru_stime.tv_usec);
@@ -42,13 +57,12 @@ int bndm_eds_mp_run(const unsigned char *teds,
     getrusage(RUSAGE_SELF, &ruse1);
 
     for (int i = 0; i < loops; i++) {
-        matches = bndm_eds_mp_search(teds, len, IUPAC_patterns[0], IUPAC_patterns[1], m*3);
+        matches = bndm_eds_mp_search(teds, len, IUPAC_patterns, num_iupac_patterns, m*3);
     }
 
     getrusage(RUSAGE_SELF, &ruse);
     ssec2 = (double) (ruse.ru_stime.tv_sec*1000000 + ruse.ru_stime.tv_usec);
     usec2 = (double) (ruse.ru_utime.tv_sec*1000000 + ruse.ru_utime.tv_usec);
-
 
     printf("Matches:\t%d\n", matches);
     printf("User time:\t%f s\n", (usec2 - usec1)/(double) 1000000);
@@ -58,214 +72,193 @@ int bndm_eds_mp_run(const unsigned char *teds,
     return matches;
 }
 
-int bndm_eds_mp_search(const unsigned char *teds,
+int bndm_eds_mp_search(const unsigned char *text,
                        const size_t len,
-                       unsigned char *pattern0,
-                       unsigned char *pattern1,
+                       unsigned char patterns[MAX_DNA_PATTERNS][MAX_PATTERN_LENGTH],
+                       unsigned int num_patterns,
                        unsigned int m) {
-    unsigned int B[2][SIGMA];
-    unsigned int S[2][SIGMA];
-    //Stores starting register values for change from BNDM to SA. Basically one-set-bit on different positions 0 to m-1.
-    unsigned int *R[2];
-    R[0] = (unsigned int *) malloc(m*sizeof(unsigned int));
-    R[1] = (unsigned int *) malloc(m*sizeof(unsigned int));
-    int i, j, F, D[2], D2[2], last, last_candidate[2], count, R1[2], R2[2];
+    assert(m%3==0);
+    assert(m <= MAX_PATTERN_LENGTH);
+    assert(num_patterns <= MAX_DNA_PATTERNS);
 
-    /* BADM Preprocessing */
-    for (i = 0; i < SIGMA; i++) {
-        B[0][i] = 0;
-        B[1][i] = 0;
+    DEBUG_PRINT("  BNDM-EDS-AA IUPAC Pattern 0: %.*s\n", (int) m, pattern0);
+    DEBUG_PRINT("                    Pattern 1: %.*s\n", (int) m, pattern1);
+
+    int S[MAX_DNA_PATTERNS][SIGMA],
+        B[MAX_DNA_PATTERNS][SIGMA]; // Preprocessed matching vectors for both patterns, S for ShiftAND, B for BNDM
+    // int R[MAX_PATTERN_LENGTH];
+    int F; // Preprocessed masking vector to verify full matches or prefix matches from matching vectors D[*]
+
+    const unsigned int STATE_VECTOR_MERGE_MASK = 0x24924924U; // ShiftAnd Dual-Pattern Mask
+    // const unsigned long int STATE_VECTOR_MERGE_MASK = 0x4924924924924924UL; // For 64bit matching
+
+    int D[MAX_DNA_PATTERNS]; // Matching vectors used for both SA and BNDM
+    int D2[MAX_DNA_PATTERNS
+    ]; // Matching vector used to save state from BNDM prefix match to be used later in SA matching
+    int R1[MAX_DNA_PATTERNS], R2[MAX_DNA_PATTERNS]; // Matching vectors used to store state between segments
+
+    /* BNDM Preprocessing */
+    for (int i = 0; i < SIGMA; i++) {
+        for (unsigned int p = 0; p < NUM_PATTERNS; p++) {
+            B[p][i] = 0;
+        }
     }
     F = 1;
-    for (i = m - 1; i >= 0; i--) {
-
+    for (int i = m - 1; i >= 0; i--) {
         for (int b = 0; b < BASES; b++) {
-            B[0][IUPAC_SYMBOLS_TO_BASES[pattern0[i]][b]] |= F;
-            B[1][IUPAC_SYMBOLS_TO_BASES[pattern1[i]][b]] |= F;
+            for (unsigned int p = 0; p < NUM_PATTERNS; p++) {
+                B[p][IUPAC_SYMBOLS_TO_BASES[patterns[p][i]][b]] |= F;
+            }
         }
-        R[0][i] = F;
-        R[1][i] = F;
         F <<= 1;
     }
     F >>= 1;
 
-    /*SA Preprocessing*/
-    for (i = 0; i < SIGMA; ++i) {
-        S[0][i] = 0;
-        S[1][i] = 0;
+    /* SA Preprocessing */
+    for (int i = 0; i < SIGMA; ++i) {
+        for (unsigned int p = 0; p < NUM_PATTERNS; p++) {
+            S[p][i] = 0;
+        }
     }
-    for (i = 0, j = 1; i < m; ++i, j <<= 1) {
+    for (int i = 0, j = 1; i < m; ++i, j <<= 1) {
         for (int b = 0; b < BASES; b++) {
-            S[0][IUPAC_SYMBOLS_TO_BASES[pattern0[i]][b]] |= j;
-            S[1][IUPAC_SYMBOLS_TO_BASES[pattern1[i]][b]] |= j;
+            for (unsigned int p = 0; p < NUM_PATTERNS; p++) {
+                S[p][IUPAC_SYMBOLS_TO_BASES[patterns[p][i]][b]] |= j;
+            }
         }
     }
 
     /* Searching */
-    unsigned int elementNum;
-    unsigned int elementLength;
-    unsigned int elementStart, elementEnd;
-    unsigned int segmentCounter = 0;
-    unsigned int elementCounter = 0;
-    count = 0;
-    R1[0] = R2[0] = D2[0] = 0;
-    R1[1] = R2[1] = D2[1] = 0;
+    for (unsigned int p = 0; p < NUM_PATTERNS; p++) {
+        R1[p] = R2[p] = D2[p] = 0;
+    }
+    unsigned int matches = 0, segmentCounter = 0, elementCounter = 0;
 
     int aPointer = 0;
     while (aPointer < len) {
-        //Processing the beginning of a segment
-        aPointer += byteDecodeInt(teds + aPointer, &elementNum);
+        // Processing the beginning of a segment
+        unsigned int segmentSize;
+        aPointer += byteDecodeInt(text + aPointer, &segmentSize);
         segmentCounter++;
-        elementCounter += elementNum;
-        R1[0] = R2[0];
-        R1[1] = R2[1];
-        R2[0] = 0;
-        R2[1] = 0;
+        elementCounter += segmentSize;
+        for (unsigned int p = 0; p < NUM_PATTERNS; p++) {
+            R1[p] = R2[p];
+            R2[p] = 0;
+        }
+        DEBUG_PRINT(" SEGMENT segment=%d, elements=%d, totalElements=%d, aPointer=%d\n",
+                    segmentCounter, segmentSize, elementCounter, aPointer);
 
-        //Process one element of the segment.
-        for (unsigned int k = 0; k < elementNum; k++) {
-            aPointer += byteDecodeInt(teds + aPointer, &elementLength);
-            elementStart = aPointer;
-            elementEnd = elementStart + elementLength;
-            DEBUG_PRINT("  ELEMENT segment = %d, element = %d/%d, start = %d, end = %d, len = %d, elementCounter = %d\n",
-                        segmentCounter, k, elementNum, elementStart, elementEnd, elementLength, elementCounter);
+        // Process one element of the segment.
+        for (unsigned int k = 0; k < segmentSize; k++) {
+            unsigned int elementLength;
+            aPointer += byteDecodeInt(text + aPointer, &elementLength);
+            unsigned int elementStart = aPointer;
+            unsigned int elementEnd = elementStart + elementLength;
+            DEBUG_PRINT(
+                "  ELEMENT segment = %d, element = %d/%d, start = %d, end = %d, len = %d, elementCounter = %d\n",
+                segmentCounter, k, segmentSize, elementStart, elementEnd, elementLength, elementCounter);
 
-            //Perform SA search at the beginning of the element.
-            j = elementStart;
-            D[0] = R1[0];
-            D[1] = R1[1];
-            while (j < (elementStart + m) && j < elementEnd) {
-                char curr_symbol = teds[j];
-                D[0] = ((D[0] << 1) | 1) & S[0][curr_symbol];
-                D[1] = ((D[1] << 1) | 1) & S[1][curr_symbol];
-                DEBUG_PRINT("    SA1 j=%d, curr_symbol=%c, D[0]=0x%x, D[1]=0x%x\n", j, curr_symbol, D[0], D[1]);
-
-                if (D[0] & F) {
-                    count++;
-                    DEBUG_PRINT(
-                        "      SA HIT (pattern0): j = %d, c = %c, D[0] = 0x%x, S = 0x%x, elementStart = %d, m = %d, R1 = 0x%x\n",
-                        j,
-                        curr_symbol,
-                        D[0],
-                        S[0][curr_symbol],
-                        elementStart,
-                        m,
-                        R1[0]);
-                }
-                if (D[1] & F) {
-                    count++;
-                    DEBUG_PRINT(
-                        "      SA HIT (pattern1): j = %d, c = %c, D[1] = 0x%x, S = 0x%x, elementStart = %d, m = %d, R1 = 0x%x\n",
-                        j,
-                        curr_symbol,
-                        D[1],
-                        S[1][curr_symbol],
-                        elementStart,
-                        m,
-                        R1[1]);
-                }
-                j++;
+            // Perform SA search at the beginning of the element.
+            for (unsigned int p = 0; p < NUM_PATTERNS; p++) {
+                D[p] = R1[p];
             }
-            if (elementLength < m)
-                goto element_end;
+            for (int j = elementStart; j < (elementStart + m) && j < elementEnd; j++) {
+                char curr_symbol = text[j];
+                for (unsigned int p = 0; p < NUM_PATTERNS; p++) {
+                    D[p] = ((D[p] << 1) | 1) & S[p][curr_symbol];
+                }
+                DEBUG_PRINT("    SA1 j=%d, curr_symbol=%c, D0=0x%x, D1=0x%x\n", j, curr_symbol, D0, D1);
 
-            //Perform BNDM search.
-            j = elementStart;
-            while (j + m - 1 < elementEnd) {
-                D2[0] = 0;
-                D2[1] = 0;
-                last = m;
-                last_candidate[0] = last;
-                last_candidate[1] = last;
-                i = m - 1;
-                D[0] = ~0;
-                D[1] = ~0;
-                DEBUG_PRINT("    BNDM j=%d, i=%d\n", j, i);
-                while (i >= 0 && (D[0]!=0 || D[1]!=0)) {
-                    char curr_symbol = teds[j + i];
-                    D[0] &= B[0][curr_symbol];
-                    D[1] &= B[1][curr_symbol];
-                    DEBUG_PRINT("      BNDM j=%d, i=%d, j+i=%d, curr_symbol=%c, D[0]=0x%x, D[1]=0x%x\n",
-                                j,
-                                i,
-                                j + i,
-                                curr_symbol,
-                                D[0],
-                                D[1]);
-                    i--;
-
-                    int update_last = 0;
-                    if (D[0]!=0 && (D[0] & F)!=0) {
-                        if (i >= 0) { // Pure pattern prefix
-                            last_candidate[0] = i + 1;
-                            D2[0] |= R[0][last_candidate[0]];
-                        } else { // Match
-                            count++;
-                            DEBUG_PRINT("        BNDM HIT (pattern0): j = %d, i = %d, c = %c, D = 0x%x, B = 0x%x\n",
-                                   j,
-                                   i,
-                                   curr_symbol,
-                                   D[0],
-                                   B[0][curr_symbol]);
-                        }
+                int match = 0;
+                for (unsigned int p = 0; p < NUM_PATTERNS; p++) {
+                    match |= (D[p] & F);
+                }
+                if (match) {
+                    matches++;
+                    DEBUG_PRINT("      MATCH (p0): j=%d, c=%c, D0=0x%x, S=0x%x, elementStart=%d, m=%lu, R1=0x%x\n",
+                                j, curr_symbol, D0, S0[curr_symbol], elementStart, m, R10);
+                    DEBUG_PRINT("      MATCH (p1): j=%d, c=%c, D1=0x%x, S=0x%x, elementStart=%d, m=%lu, R1=0x%x\n",
+                                j, curr_symbol, D1, S1[curr_symbol], elementStart, m, R11);
+                }
+            }
+            if (elementLength > m) {
+                // Perform BNDM search.
+                int j, last;
+                for (j = elementStart + 1; j + m - 1 < elementEnd; j += last) {
+                    for (unsigned int p = 0; p < NUM_PATTERNS; p++) {
+                        D2[p] = 0;
+                        D[p] = ~0;
                     }
-                    if (D[1]!=0 && (D[1] & F)!=0) {
-                        if (i >= 0) {
-                            last_candidate[1] = i + 1;
-                            D2[1] |= R[1][last_candidate[1]];
-                        } else {
-                            count++;
-                            DEBUG_PRINT("        BNDM HIT (pattern1): j = %d, i = %d, c = %c, D = 0x%x, B = 0x%x\n",
-                                   j,
-                                   i,
-                                   curr_symbol,
-                                   D[1],
-                                   B[1][curr_symbol]);
+                    int D_OR = ~0;
+                    last = m;
+                    DEBUG_PRINT("    BNDM j=%d, D2=0x%x, D3=0x%x, last=%d, text=%.*s\n",
+                                j, D2, D3, last, (int) m, text + j);
+                    for (int i = m - 1; i >= 0 && D_OR; --i) {
+                        char curr_symbol = text[j + i];
+                        for (unsigned int p = 0; p < NUM_PATTERNS; p++) {
+                            D[p] = D[p] & B[p][curr_symbol];
                         }
+
+                        DEBUG_PRINT("      BNDM j=%d, i=%d, j+i=%d, curr_symbol=%c, (m-1-i)mod3=%lu, "
+                                    "D0=0x%x, DC0=0x%x,B0[curr_symbol]=0x%x, "
+                                    "D1=0x%x, DC1=0x%x, B1[curr_symbol]=0x%x, D1 | DC1 = %x\n",
+                                    j, i, j + i, curr_symbol, (m - 1 - i)%3, D0, DC0, B0[curr_symbol], D1,
+                                    DC1,
+                                    B1[curr_symbol], D1 | DC1);
+
+                        for (unsigned int p = 0; p < NUM_PATTERNS; p++) {
+                            if ((D[p] & F)!=0 && i > 0) {
+                                last = i;
+                                D2[p] |= 1 << (m - 1 - i); // D2 |= 1 << R[last];
+                                DEBUG_PRINT("        BNDM Prefix: j=%d, i=%d, curr_symbol=%c, last=%d, D2=0x%x\n",
+                                            j, i, curr_symbol, last, D2);
+                            }
+                        }
+                        int D_OR = 0;
+                        for (unsigned int p = 0; p < NUM_PATTERNS; p++) {
+                            D_OR |= D[p];
+                        }
+                        if ((D_OR & F)!=0 && i==0) {
+                            matches++;
+                            DEBUG_PRINT("         MATCH (p0): j=%d, i=%d, c=%c, D=0x%x, B=0x%x\n",
+                                        j, i, curr_symbol, D0, B0[curr_symbol]);
+                            DEBUG_PRINT("         MATCH (p1): j=%d, i=%d, c=%c, D=0x%x, B=0x%x\n",
+                                        j, i, curr_symbol, D1, B1[curr_symbol]);
+                        }
+                        for (unsigned int p = 0; p < NUM_PATTERNS; p++) {
+                            D[p] = D[p] << 1;
+                        }
+                        D_OR = D_OR << 1;
                     }
-                    D[0] <<= 1;
-                    D[1] <<= 1;
+                    DEBUG_PRINT("    BNDM last=%d, m=%lu\n", last, m);
                 }
-                last = min(last_candidate[0], last_candidate[1]);
-                j += last;
-            }
-            //Perform SA search at the end of the element.
-            DEBUG_PRINT("  BNDM->SA2 j=%d last=%d, jnext = %d, D[0]=0x%x, D[1]=0x%x\n",
-                        j,
-                        last,
-                        j + m - last,
-                        D2[0],
-                        D2[1]);
-            j += m - last; //Moving j pointer to the initial position for SA.
-            D[0] = D2[0]; //Setting the initial value to SA register.
-            D[1] = D2[1]; //Setting the initial value to SA register.
-            while (j < elementEnd) {
-                char curr_symbol = teds[j];
-                D[0] = ((D[0] << 1) | 1) & S[0][curr_symbol];
-                D[1] = ((D[1] << 1) | 1) & S[1][curr_symbol];
-                DEBUG_PRINT("    SA2 j=%d, curr_symbol=%c, D[0]=0x%x, D[1]=0x%x\n", j, curr_symbol, D[1], D[2]);
 
-                if (D[0] & F) {
-                    count++;//This cannot happen...
-                    DEBUG_PRINT("      SA2 HIT (pattern0): j = %d, D[0] = 0x%x\n", j, D[0]);
+                // Perform SA search at the end of the element.
+                for (unsigned int p = 0; p < NUM_PATTERNS; p++) {
+                    D[p] = D2[p]; // Setting the initial value to SA register.
                 }
-                if (D[1] & F) {
-                    count++;//This cannot happen...
-                    DEBUG_PRINT("      SA2 HIT (pattern1): j = %d, D[1] = 0x%x\n", j, D[1]);
+                DEBUG_PRINT("  BNDM->SA2 j=%d, D2=0x%x, D3=0x%x\n", j, D2, D3);
+
+                for (j = j + m - last; j < elementEnd; j++) {
+                    char curr_symbol = text[j];
+                    for (unsigned int p = 0; p < NUM_PATTERNS; p++) {
+                        D[p] = ((D[p] << 1) | 1) & S[p][curr_symbol];
+                    }
+                    DEBUG_PRINT("    SA2 j=%d, curr_symbol=%c, D0=0x%x, D1=0x%x\n", j, curr_symbol, D0, D1);
                 }
-                j++;
             }
 
-            element_end:
-            R2[0] |= D[0]; //Merge the SA registers of single elements.
-            R2[1] |= D[1]; //Merge the SA registers of single elements.
+            // Merge the SA registers of individual elements into a cumulative register for the whole segment
+            for (unsigned int p = 0; p < NUM_PATTERNS; p++) {
+                R2[p] |= D[p];
+            }
 
-            //Set the pointer to the beginning of the next element.
+            // Set the pointer to the beginning of the next element.
             aPointer += elementLength;
         }
     }
 
-    DEBUG_PRINT("count = %d, segments = %d, elements = %d\n", count, segmentCounter, elementCounter);
+    DEBUG_PRINT("BNDM-EDS-AA Matches=%d, segments=%d, elements=%d\n", matches, segmentCounter, elementCounter);
 
-    return count;
+    return matches;
 }
--- src/bndm_eds_mp.h
+++ src/bndm_eds_mp.h
@@ -1,13 +1,13 @@
 #include <stddef.h>
 
-int bndm_eds_mp_search(const unsigned char *teds,
+int bndm_eds_mp_search(const unsigned char *text,
                        const size_t len,
-                       unsigned char *pattern0,
-                       unsigned char *pattern1,
+                       unsigned char patterns[MAX_DNA_PATTERNS][MAX_PATTERN_LENGTH],
+                       unsigned int num_patterns,
                        unsigned int m);
 
 int bndm_eds_mp_run(const unsigned char *teds,
                     const size_t len,
-                    const unsigned char *pattern,
+                    const unsigned char *aa_pattern,
                     const size_t m,
                     const int loops);
